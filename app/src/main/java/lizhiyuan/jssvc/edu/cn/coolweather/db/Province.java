dc4cfe05c5d822f032b6209ece84d021dc17c95c
C515 <C515>
2018-01-02 10:42:48 +0800

    14.3 finish

---
+++ app/src/main/java/lizhiyuan/jssvc/edu/cn/coolweather/db/Province.java
@@ -0,0 +1,40 @@
+package lizhiyuan.jssvc.edu.cn.coolweather.db;
+
+/**
+ * Created by C515 on 2018/1/2.
+ */
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
